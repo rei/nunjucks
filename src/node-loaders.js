84c1d40eb1b3e94c60ee3d8f724a99969f9f2518
Alex Perkins <aperkin@rei.com>
2016-07-25 12:07:05 -0700

    Removing chokidar/watcher code

--- src/node-loaders.js
+++ src/node-loaders.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var path = require('path');
 var lib = require('./lib');
 var Loader = require('./loader');
-var chokidar = require('chokidar');
 
 // Node <0.7.1 compatibility
 var existsSync = fs.existsSync || path.existsSync;
@@ -21,23 +20,6 @@ var FileSystemLoader = Loader.extend({
         }
         else {
             this.searchPaths = ['.'];
-        }
-
-        if(!noWatch) {
-            // Watch all the templates in the paths and fire an event when
-            // they change
-            lib.each(this.searchPaths, function(p) {
-                if(existsSync(p)) {
-                    var watcher = chokidar.watch(p);
-
-                    watcher.on('all', function(event, fullname) {
-                        fullname = path.resolve(fullname);
-                        if(event === 'change' && fullname in this.pathsToNames) {
-                            this.emit('update', this.pathsToNames[fullname]);
-                        }
-                    }.bind(this));
-                }
-            }.bind(this));
         }
     },
 
